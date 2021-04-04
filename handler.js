0305cc03a2414ec7a5a741e6bb7f3c725de69f99
shirish <shirish101k@gmail.com>
2021-04-03 19:36:17 -0500

    saving work, askTesla working

--- handler.js
+++ handler.js
@@ -5,78 +5,68 @@ AWS.config.update({region: 'us-east-1'});
 const dynamo = new AWS.DynamoDB({apiVersion: "2012-08-10"});
 const tesla_sdk = require('./tesla-sdk');
 
-const CHATCONNECTION_TABLE = 'TeslaBotTable';
+const CHAT_CONNECTION_TABLE = 'TeslaBotTable';
 
-const successfullResponse = {
+const successfulResponse = {
   statusCode: 200,
   body: 'everything is alright'
 };
 
 module.exports.connectionHandler = async (event, context, callback) => {
-  console.log(event);
+  console.log(JSON.stringify(event));
   try {
     if (event.requestContext.eventType === 'CONNECT') {
       // Handle connection
       await addConnection(event);
-      callback(null, successfullResponse);
+      callback(null, successfulResponse);
     } else if (event.requestContext.eventType === 'DISCONNECT') {
       // Handle disconnection
       await deleteConnection(event.requestContext.connectionId);
-      callback(null, successfullResponse);
+      callback(null, successfulResponse);
     }
   } catch (err) {
-    console.log(err.toString());
+    console.log(JSON.stringify(err));
     callback(null, JSON.stringify(err));
   }
 };
 
-// THIS ONE DOESNT DO ANYHTING
 module.exports.defaultHandler = async (event, context, callback) => {
-  console.log('defaultHandler was called');
-  console.log(event);
-
-  callback(null, {
-    statusCode: 200,
-    body: 'defaultHandler'
-  });
-};
-
-module.exports.sendMessageHandler = async (event, context, callback) => {
+  console.log(JSON.stringify(event));
+  const connectionId = event.requestContext.connectionId;
   try {
-    console.log("event before sendMessage  " + event.toString());
-    await sendMessageToConnection(event);
-    callback(null, successfullResponse);
+    const body = JSON.parse(event.body);
+    const inputMessage = body.data;
+    const { action } = body;
+    const responseData = await customHandlers[action](connectionId, inputMessage);
+    await send(event, connectionId, responseData);
+    callback(null, successfulResponse);
   } catch (err) {
+    await send(event, connectionId, JSON.stringify(err));
     callback(null, JSON.stringify(err));
   }
-}
+};
 
-const sendMessageToConnection = async (event) => {
-  const connectionId = event.requestContext.connectionId;
-  const body = JSON.parse(event.body);
-  const postData = body.data;
+const moreTeslaHandler = async (connectionId, inputMessage) => {
   try {
-    // TODO: Insert DialogFlow SDK here which calls corresponding tesla SDK method.
-    const tableData = await getItemWithConnectionId(connectionId);
-    console.log("table data : " + tableData.toString());
-    const { Item } = tableData;
+    const connectionObj = await getItemWithConnectionId(connectionId);
+    const { Item } = connectionObj;
     const { authToken } = Item;
     const { S: authTokenVal } = authToken;
+    // TODO: Insert DialogFlow SDK here which calls corresponding tesla SDK method.
     const vehicleInfo = await tesla_sdk.vehicleDetails(authTokenVal);
-    const data = {
+    return {
       vehicleInfo,
-      customMessage: postData
+      inputMessage
     };
-    return send(event, connectionId, data);
-  } catch (e) {
-    console.log("error in send message function" + e.toString());
-    throw new Error(e.toString());
+  } catch (err) {
+    console.log("error in moreTeslaHandler" + JSON.stringify(err));
+    throw new Error(JSON.stringify(err));
   }
 }
 
 const getItemWithConnectionId = async (connectionId) => {
   const params = {
-    TableName: CHATCONNECTION_TABLE,
+    TableName: CHAT_CONNECTION_TABLE,
     Key: {
       "connectionId": {
         S: connectionId
@@ -86,8 +76,7 @@ const getItemWithConnectionId = async (connectionId) => {
   return await dynamo.getItem(params).promise();
 }
 
-const send = (event, connectionId, data) => {
-  console.log("data in send" + data.toString());
+const send = async (event, connectionId, data) => {
   const endpoint = event.requestContext.domainName + "/" + event.requestContext.stage;
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
     apiVersion: "2018-11-29",
@@ -98,7 +87,7 @@ const send = (event, connectionId, data) => {
     ConnectionId: connectionId,
     Data: JSON.stringify(data)
   };
-  return apigwManagementApi.postToConnection(params).promise();
+  return await apigwManagementApi.postToConnection(params).promise();
 };
 
 const addConnection = async (event) => {
@@ -111,7 +100,7 @@ const addConnection = async (event) => {
   const authToken = headers['authToken'];
 
   const params = {
-    TableName: CHATCONNECTION_TABLE,
+    TableName: CHAT_CONNECTION_TABLE,
     Item: {
       connectionId: {
         S: connectionId
@@ -127,7 +116,7 @@ const addConnection = async (event) => {
 
 const deleteConnection = async (connectionId) => {
   const params = {
-    TableName: CHATCONNECTION_TABLE,
+    TableName: CHAT_CONNECTION_TABLE,
     Key: {
       connectionId: {
         S: connectionId
@@ -137,3 +126,7 @@ const deleteConnection = async (connectionId) => {
 
   return await dynamo.deleteItem(params).promise();
 };
+
+const customHandlers = {
+  askTesla: moreTeslaHandler
+}
